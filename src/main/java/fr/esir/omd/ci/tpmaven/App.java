bfe8c4bab0e79dfb12d6e47eccaa10de0fc50736
unknown <lgblmp7@gmail.com>
2022-10-07 08:41:07 +0200

    initial commité

---
+++ src/main/java/fr/esir/omd/ci/tpmaven/App.java
@@ -0,0 +1,16 @@
+package fr.esir.omd.ci.tpmaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+  /**
+   * 
+   * @param args
+   */
+  public static void main(final String[] args) {
+    int a = 8;
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/fr/esir/omd/ci/tpmaven/FirstPdfTest.java
@@ -0,0 +1,15 @@
+package fr.esir.omd.ci.tpmaven;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class FirstPdfTest {
+	
+	@Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+}
